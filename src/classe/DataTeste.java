17e83466efb968a749dbdd7a1476a0c57c929eb5
stefanysilveira <stefany.silveira21@gmail.com>
2022-06-28 14:35:08 -0300

    desafio @ construtor

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -4,14 +4,9 @@ public class DataTeste {
     public static void main(String[] args) {
 
         Data d1 = new Data();
-        d1.dia = 21;
-        d1.mes = 10;
         d1.ano = 2000;
 
-        Data d2 = new Data();
-        d2.dia = 15;
-        d2.mes = 07;
-        d2.ano = 1992;
+        Data d2 = new Data(15, 07, 1992);
 
         String dataFormatada1 = d1.obterDataFormata();
 
