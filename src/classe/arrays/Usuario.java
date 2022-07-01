f42d4c680c71277812bf0d6d7b16c4ca2ecaca24
stefanysilveira <stefany.silveira21@gmail.com>
2022-07-01 14:49:09 -0300

    exemplo @ equals

---
+++ src/classe/arrays/Usuario.java
@@ -0,0 +1,21 @@
+package classe.arrays;
+
+public class Usuario {
+
+    public String nome;
+    public String email;
+
+    public boolean equals(Object objeto){
+
+        if (objeto instanceof Usuario){
+            Usuario outro = (Usuario) objeto;
+
+            boolean nomeIgual = outro.nome.equals(this.nome);
+            boolean emailIgual = outro.email.equals(this.email);
+
+            return nomeIgual && emailIgual;
+        } else {
+            return false;
+        }
+    }
+}
