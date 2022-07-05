ba69c2c406a70ef46b600a5b35202e008fcf6846
stefanysilveira <stefany.silveira21@gmail.com>
2022-07-05 19:16:12 -0300

    exemplo @ Enum

---
+++ src/oo/composicao/heranca/Jogador.java
@@ -0,0 +1,24 @@
+package oo.composicao.heranca;
+
+public class Jogador {
+
+    int x;
+    int y;
+
+    boolean andar(Direcao direcao){
+        switch (direcao){
+            case NORTE:
+                y--;
+                break;
+            case LESTE:
+                x++;
+                break;
+            case SUL:
+                y++;
+            case OESTE:
+                x--;
+            break;
+        }
+        return true;
+    }
+}
