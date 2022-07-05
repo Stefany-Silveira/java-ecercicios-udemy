d50d61d79fb22185df5fcf9f3b49428b9fff681a
stefanysilveira <stefany.silveira21@gmail.com>
2022-07-04 21:13:47 -0300

    desafio @ Composição

---
+++ src/oo/composicao/desafio/Produto.java
@@ -0,0 +1,14 @@
+package oo.composicao.desafio;
+
+import javax.lang.model.element.ModuleElement;
+
+public class Produto {
+
+    String nome;
+    double preco;
+
+    Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
