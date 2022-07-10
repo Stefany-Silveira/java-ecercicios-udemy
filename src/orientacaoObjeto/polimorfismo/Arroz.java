ec4404efa32197e3d99740da43b03e3e57b05712
stefanysilveira <stefany.silveira21@gmail.com>
2022-07-10 14:44:13 -0300

    exemplo @ Polimorfismo

---
+++ src/orientacaoObjeto/polimorfismo/Arroz.java
@@ -0,0 +1,18 @@
+package orientacaoObjeto.polimorfismo;
+
+public class Arroz {
+
+    private double peso;
+
+    public Arroz(double peso){
+        setPeso(peso);
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
---
+++ src/orientacaoObjeto/polimorfismo/Feijao.java
@@ -0,0 +1,18 @@
+package orientacaoObjeto.polimorfismo;
+
+public class Feijao {
+
+    private double peso;
+
+    public Feijao(double peso){
+        setPeso(peso);
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
