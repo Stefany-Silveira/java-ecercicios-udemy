1b7477e91a6432a86dc83e5095d91fe342ea23b7
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-03 10:23:32 -0300

    exemplo @ Básico SEM Generics

---
+++ src/generics/CaixaObjeto.java
@@ -0,0 +1,16 @@
+package generics;
+
+import javax.management.ObjectName;
+
+public class CaixaObjeto {
+
+    private Object coisa;
+
+    public void guardar(Object coisa) {
+        this.coisa = coisa;
+    }
+
+    public Object abrir() {
+        return coisa;
+    }
+}
